d8337964fda3d4a088212c585b679da7609b0461
bearclimber <vincentchsieh@gmail.com>
2017-08-04 14:36:11 -0400

    beautify

--- routes/page_a.js
+++ routes/page_a.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-  	title: '华人生活网',
+  res.render('page_a', { 
 	partials: {
 		header: '../views/partials/header',
 		navbar: '../views/partials/navbar'
