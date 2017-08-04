f2a2c301784e0efb29cce1502584b49edb25484d
bearclimber <vincentchsieh@gmail.com>
2017-08-04 19:47:25 -0400

    a lot of stuff

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { 
   	title: '华人生活网',
 	partials: {
-		index_header: '../views/partials/index_header',
-		navbar: '../views/partials/navbar'
+		header: '../views/partials/header',
+		navbar: '../views/partials/navbar',
+		states: '../views/partials/states'
 	}
 	})
 });
