c3745c0de77fabca5501bfd70d1da745d630e266
bearclimber <vincentchsieh@gmail.com>
2017-08-04 16:18:41 -0400

    finished top

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { 
   	title: '华人生活网',
 	partials: {
-		header: '../views/partials/header',
+		index_header: '../views/partials/index_header',
 		navbar: '../views/partials/navbar'
 	}
 	})
