f8cfc2269a34ba1a6954dc02477cf54da12edcce
Brainify1 <brianchew357@gmail.com>
2017-08-10 16:34:21 -0400

    changes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { 
   	title: '华人生活网',
+  	isLoggedIn: req.isAuthenticated(),
 	partials: {
 		header: '../views/partials/header',
 		navbar: '../views/partials/navbar',
-		states: '../views/partials/states'
+		states: '../views/partials/states',
 	}
 	})
 });
