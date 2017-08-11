d0349df0ccae28538e576d3a3b3b78a3f1667bd8
Brainify1 <brianchew357@gmail.com>
2017-08-10 21:32:38 -0400

    upload file

--- routes/postList.js
+++ routes/postList.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs')
+var db = mongojs('mongodb://localhost:27017/test');
+var listingCollection = db.collection('listings');
+var multer  = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/upload/listings/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+  }
+})
+
+var upload = multer({ storage })
 
 function loginRequired(req,res,next){
 	if(!req.isAuthenticated()){
@@ -20,5 +34,14 @@ router.get('/', loginRequired, function(req, res, next) {
 	}
 	})
 });
-
+router.post('/', upload.single('images'), function(req, res, next) {
+	var listing = req.body;
+	listing.img = req.file;
+	listingCollection.save(listing, function(err, newListing) {
+		if (err) {
+			return err
+		}
+		res.json(newListing)
+	})
+})
 module.exports = router;
