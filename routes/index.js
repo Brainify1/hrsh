bd4f2a489670f0f37a69794fbf2095c7aeaaa61e
bearclimber <vincentchsieh@gmail.com>
2017-08-12 15:03:16 -0400

    d

--- routes/index.js
+++ routes/index.js
@@ -8,41 +8,104 @@ router.get('/', function(req, res, next){
   res.redirect('/ny');
 });
 
+var allStates = ['ny', 'la', 'van', 'sea', 'chi', 'lv', 'hou', 'bos', 'sfr', 'was', 'sdi', 'syd', 'phi', 'hwi', 'atl', 'dal', 'flo']
+
+var allCategory = ['news', 'videos', 're', 'secondHand', 'travel', 'law', 'education', 'hire', 'service']
 
 /* GET home page. */
 router.get('/:states', function(req, res, next) {
   var states = req.params.states;
-  res.render('index', {
-    title : states,
-    partials : {
-    	head: '../views/partials/head',
-      header: '../views/partials/header',
-      navbar: '../views/partials/navbar',
-      states: '../views/partials/states',
-      footer: '../views/partials/footer',
-      scripts: '../views/partials/scripts'
-    }
-  })
+  if (allStates.indexOf(states) === -1) {
+    res.send('Wrong path! Please go back to previus page.');
+  } else {
+    res.render('index', {
+      title : '华人生活网',
+      link : states,
+      partials : {
+        head: '../views/partials/head',
+        header: '../views/partials/header',
+        navbar: '../views/partials/navbar',
+        states: '../views/partials/states',
+        footer: '../views/partials/footer',
+        scripts: '../views/partials/scripts'
+      }
+    })
+  }
 });
 
 
 
-router.get('/:states/:listings', function(req, res, next) {
+router.get('/:states/:category', function(req, res, next) {
   var states = req.params.states;
-  var listings = req.params.listings;
-  // res.send('omg' + states + listings);
-  res.render('realEstate', {
-    title : states,
-    partials : {
-    	head: '../views/partials/head',
-      header: '../views/partials/header',
-      navbar: '../views/partials/navbar',
-      states: '../views/partials/states',
-      footer: '../views/partials/footer',
-      scripts: '../views/partials/scripts'
-    }
-  })
-})
+  var category = req.params.category;
+  if (allStates.indexOf(states) === -1 | allCategory.indexOf(category) === -1) {
+    res.send('Wrong path! Please go back to previus page.');
+  } else if (allCategory.indexOf(category) === 0) {
+    res.render('news', {
+      title : '华人生活网',
+      link : states,
+      categ: category,
+      partials : {
+        head: '../views/partials/head',
+        header: '../views/partials/header',
+        navbar: '../views/partials/navbar',
+        states: '../views/partials/states',
+        footer: '../views/partials/footer',
+        scripts: '../views/partials/scripts'
+      }
+    })
+  } else if (allCategory.indexOf(category) === 1) {
+    res.render('videos', {
+      title : '华人生活网',
+      link : states,
+      categ: category,
+      partials : {
+        head: '../views/partials/head',
+        header: '../views/partials/header',
+        navbar: '../views/partials/navbar',
+        states: '../views/partials/states',
+        footer: '../views/partials/footer',
+        scripts: '../views/partials/scripts'
+      }
+    })
+  } else {
+    res.render('category', {
+      title : '华人生活网',
+      link : states,
+      categ : category,
+      partials : {
+        head: '../views/partials/head',
+        header: '../views/partials/header',
+        navbar: '../views/partials/navbar',
+        states: '../views/partials/states',
+        footer: '../views/partials/footer',
+        scripts: '../views/partials/scripts'
+      }
+    })
+  }
+});
+
+router.get('/:states/:category/:action', function(req, res, next) {
+  var states = req.params.states;
+  var category = req.params.category;
+  if (allStates.indexOf(states) === -1 | allCategory.indexOf(category) === -1) {
+    res.send('Wrong path! Please go back to previus page.');
+  } else {
+    res.render('postList', {
+      title : '华人生活网',
+      link : states,
+      categ : category,
+      partials : {
+        head: '../views/partials/head',
+        header: '../views/partials/header',
+        navbar: '../views/partials/navbar',
+        states: '../views/partials/states',
+        footer: '../views/partials/footer',
+        scripts: '../views/partials/scripts'
+      }
+    })
+  }
+});
 
 
 
