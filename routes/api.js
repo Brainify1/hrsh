35dc775ab7f445ef617938b24f7d8cc58f047c36
bearclimber <vincentchsieh@gmail.com>
2017-08-13 16:16:07 -0400

    d

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,14 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost:27017/test', ['listings']);
 var listingCollection = db.collection('listings');
+var videosCollection = db.collection('videos');
 
-router.get('{{states}}/news', function(req, res, next) {
-	
+router.get(':states/:category', function(req, res, next) {
+	console.log(123);
+
+	videoCollection.find({ name : video1}), function(err, doc) {
+	}
+	console.log(doc);
 })
 
 
