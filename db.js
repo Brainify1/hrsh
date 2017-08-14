28061626d53ac4fc678eccb6b25b59f97807d38b
Brainify1 <brianchew357@gmail.com>
2017-08-14 17:28:15 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 var db;
-MongoClient.connect("mongodb://localhost:27017/test", (err,connection) => {
+MongoClient.connect("mongodb://Brainify1:hrsh123@ds163681.mlab.com:63681/hrshdatabase", (err,connection) => {
 	if(err){ console.log(err) }
 	
 	var db = connection;
