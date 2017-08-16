169c3f1a4b40d16ccd16b725d27001c8db53bad7
mxdi9i7 <peter.zheng88228@gmail.com>
2017-08-16 13:47:53 -0400

    news upload

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,20 @@ var mongojs = require('mongojs');
 var db = mongojs('mongodb://localhost:27017/test', ['listings']);
 var listingCollection = db.collection('listings');
 var videosCollection = db.collection('videos');
-
-router.get(':states/:category', function(req, res, next) {
+var newsCollection = db.collection('news');
+router.get('/:states/:category', function(req, res, next) {
 	console.log(123);
 
 	videoCollection.find({ name : video1}), function(err, doc) {
 	}
 	console.log(doc);
+})
+router.post('/postNews', function(req, res, next) {
+	const news = req.body;
+	console.log(news)
+	newsCollection.save(news, function(err, newsDoc) {
+		res.json(newsDoc)
+	})
 })
 
 
