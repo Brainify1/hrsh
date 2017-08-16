eec3a446f738ab0ee2fe6e0dbf5a6e8480f8207d
ssysm <shengming216qhs@gmail.com>
2017-08-17 04:09:38 +0800

    push video manager

---
+++ public/javascripts/manager.js
@@ -0,0 +1,18 @@
+﻿function videoManager() {
+    $.ajax({
+        url: "/api/manager/video/all/?key=YGJnH2mWYHw4CU3pJEQGjdNzVeL9qKfm", success: function (result) {
+            //for (var i = 0; result.length <i; i++) {
+                $('#video_display').html('')
+                $('#video_display').append(`
+                    <tr>
+                                                        <td>gfvabjshlgfhvabjc123123asd</td>
+                                                        <td>title</td>
+                                                        <td>View</td>
+                                                        <td>Date</td>
+                                                        <td><a href="#" onclick="videoDelete">删除视频</a></td>
+                                                    </tr>
+                `)
+           //}
+        }
+    })
+}
